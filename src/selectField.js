3dd18e0b7849adef12c16af60a35bb78a33b60a5
fibo <casati_gianluca@yahoo.it>
2017-04-05 19:02:25 +0200

    parse casted fields

--- src/selectField.js
+++ src/selectField.js
@@ -1,9 +1,12 @@
 var countExpression = require('./countExpression')
-// var isDataType = require('./util/isDataType')
+var isDataType = require('./util/isDataType')
 var isFieldName = require('./util/isFieldName')
 var isNumber = require('./util/isNumber')
 var isObject = require('./util/isObject')
+var isSingleQuotedString = require('./util/isSingleQuotedString')
+var isStringNumber = require('./util/isStringNumber')
 var isStar = require('./util/isStar')
+var isString = require('./util/isString')
 
 /**
  * Map columns in a SELECT.
@@ -18,14 +21,36 @@ function selectField (field) {
 
   if (isNumber(field)) return field
 
-  // TODO check if it contains a cast.
-  if (isFieldName(field)) return field
+  if (isString(field)) {
+    // Consider that a field could be casted, for example
+    //
+    // SELECT 1::VARCHAR
+
+    var fieldName = field.split('::')[0]
+    var dataType = field.split('::')[1]
+
+    if (dataType) {
+      var fieldNameIsValid = isStringNumber(fieldName) || isSingleQuotedString(fieldName) || isFieldName(fieldName)
+
+      if (fieldNameIsValid && isDataType(dataType)) return field
+    } else {
+      if (isFieldName(fieldName)) return field
+    }
+  }
 
   if (isObject(field)) {
     if (field.COUNT) {
       return countExpression(field)
     }
   }
+
+  // Should not arrive here.
+
+  var message = 'Could not parse select field'
+  if (field) message = message + ' ' + field
+  var error = new TypeError(message)
+  error.field = field
+  throw error
 }
 
 module.exports = selectField
---
+++ test/isFieldName.js
@@ -0,0 +1,11 @@
+var test = require('tape')
+
+var isFieldName = require('../src/util/isFieldName')
+
+test('isFieldName', function (t) {
+  t.ok(isFieldName('field'), 'field')
+  t.ok(isFieldName('table.field'), 'table.field')
+  t.ok(isFieldName('schema.table.field'), 'schema.table.field')
+
+  t.end()
+})
